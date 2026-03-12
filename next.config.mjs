dd3a23ae62e267aae9b7ee6dd780e798c2afd740
kazatravel <zack.zlotnick@gmail.com>
2026-03-12 14:09:21 -0700

    fix: remove deprecated images.domains in favor of remotePatterns only

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,13 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ['images.unsplash.com'],
     remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
       {
         protocol: 'https',
         hostname: 'source.unsplash.com',
