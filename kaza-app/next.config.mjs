9b2d6b922aeeb9542e9154c511c5ae0f497b3a49
kazatravel <zack.zlotnick@gmail.com>
2026-03-12 12:47:28 -0700

    chore: ignore eslint and ts errors during vercel build to unblock deployments

--- kaza-app/next.config.mjs
+++ kaza-app/next.config.mjs
@@ -1,22 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'dist',
   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
     ignoreBuildErrors: true,
-  },
-  images: {
-    domains: ['images.unsplash.com'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'source.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
   },
 };
 
